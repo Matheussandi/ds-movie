1ae7d89d722e466b553d07688331570b50733a01
Matheus Sandi <Matheussandi@hotmail.com>
2022-01-10 21:18:52 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <h1>Olá</h1>
+    <Navbar />
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcons} from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/Matheussandi">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcons />
+                            <p className="dsmovie-contact-link">/Matheussandi</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
